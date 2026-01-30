cb06f27470b54e96686da80251ab461a40d22b42
Wiktor Gudzowski <u63_wikgud_lbn@technischools.com>
2026-01-30 11:15:37 +0100

    fix: removed minor bugs

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
-import { loadStats } from '../../shared/utils/localStorage'
+import { loadStats } from '../../shared/utils/localStorage.js'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
